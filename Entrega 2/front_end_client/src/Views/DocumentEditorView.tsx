d79466ef0d0f8d75d067dc68fde5b0bb4a126429
IvanNSBS <insbs@cin.ufpe.br>
2023-07-15 21:24:30 -0300

    Starting document editor tests

--- Entrega 2/front_end_client/src/Views/DocumentEditorView.tsx
+++ Entrega 2/front_end_client/src/Views/DocumentEditorView.tsx
@@ -1,9 +1,55 @@
 import React, { FC, ReactElement } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import AppRoutes from '../AppRoutes';
+import { styled } from 'styled-components';
 
 type DocumentEditorView = {
 
+}
+
+const TextInput = styled.div`
+    width: 100%;
+    height: 90%;
+    resize: none;
+    background: white;
+    color: black;
+
+    overflow: auto;
+    padding: 10px;
+    margin-bottom: 30px;
+`
+
+const TextContainer = styled.div`
+
+    height: max(90vh, 350px);
+    width: 750px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const EditorContainer = styled.div`
+    display: flex;
+    flex-flow: row;
+    flex-direction: column;
+
+    justify-content: space-between;
+    align-items: center;
+`
+
+const EditorHeader = styled.div`
+    width: 100%;
+    display: flex;
+    align-content: space-between;
+    justify-content: space-between;
+    margin-bottom: 20px;
+`
+
+const TextInputttt: FC<DocumentEditorView> = (props: DocumentEditorView): ReactElement => {
+    return (
+        <></>
+    )
 }
 
 const DocumentEditorView: FC<DocumentEditorView> = (props: DocumentEditorView): ReactElement => {
@@ -11,11 +57,15 @@ const DocumentEditorView: FC<DocumentEditorView> = (props: DocumentEditorView): 
     const { state } = useLocation();
 
     return (
-        <div>
-            <p>Edit {state.document_name} View</p>
-            <textarea></textarea>
-            <button onClick={() => navigate(-1)}>Voltar</button>
-        </div>
+        <EditorContainer>
+            <EditorHeader>
+                <p>Edit {state.document_name} View</p>
+                <button onClick={() => navigate(-1)}>Voltar</button>
+            </EditorHeader>
+            <TextContainer contentEditable='true'>
+                <TextInput spellCheck='false'></TextInput>
+            </TextContainer>
+        </EditorContainer>
     )
 }
 
