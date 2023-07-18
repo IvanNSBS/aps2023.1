7cfdcc7d64154a39a5d8dad259fb7249c04a043c
IvanNSBS <insbs@cin.ufpe.br>
2023-07-18 20:23:11 -0300

    Adding writeCommand to text editor

--- Entrega 2/front_end_client/src/Business/Commands/WriteCommand.ts
+++ Entrega 2/front_end_client/src/Business/Commands/WriteCommand.ts
@@ -1,12 +1,28 @@
 import { ICommand } from "./ICommand";
 
-class WriteCommand extends ICommand
+export class WriteCommand extends ICommand
 {
+    private prevState: string;
+    private newState: string;
+    private textEditorNode: HTMLDivElement;
+    private onExecuteClbk: (newState: string) => void;
+
+    constructor(textEditor: HTMLDivElement, prevState: string, newState: string, onExecute: (newState: string) => void){
+        super();
+        this.textEditorNode = textEditor;
+        this.onExecuteClbk = onExecute;
+
+        this.prevState = prevState;
+        this.newState = newState;
+    }
+
     public override execute(): void {
-        
+        this.textEditorNode.textContent = this.newState;
+        this.onExecuteClbk(this.newState);
     }
 
     public override undo(): void {
-         
+        this.textEditorNode.textContent = this.prevState;
+        this.onExecuteClbk(this.prevState);
     }
 }
--- Entrega 2/front_end_client/src/Views/DocumentEditor/TextEditor.tsx
+++ Entrega 2/front_end_client/src/Views/DocumentEditor/TextEditor.tsx
@@ -5,6 +5,7 @@ import { select_token } from '../../Business/TextEditor/TextTokenSelector';
 import SuggestGrammarHighlight from './SuggestGrammarHighlight';
 import { AppContext } from '../../AppContext';
 import { AcceptGrammarSuggestionCommand } from '../../Business/Commands/AcceptGrammarSuggestionCommand';
+import { WriteCommand } from '../../Business/Commands/WriteCommand';
 
 const TextContainer = styled.div`
   height: max(85vh, 350px);
@@ -34,88 +35,112 @@ const TextInput = styled.div`
 `
 
 const TextEditor: FC = (): ReactElement => {
-    const [tokenRect, setTokenRect] = useState<DOMRect>();
-    const [tokenRange, setTokenRange] = useState<number>(0);
-    const [tokenIndex, setTokenIndex] = useState<number>(0);
+  const [currentInnerText, setCurrentInnerText] = useState<string>("");
+  const [tokenRect, setTokenRect] = useState<DOMRect>();
+  const [tokenRange, setTokenRange] = useState<number>(0);
+  const [tokenIndex, setTokenIndex] = useState<number>(0);
 
-    const tokensInfo = useRef<TokenInfo[]>([]);
-    const self = useRef<HTMLDivElement>(null);
-    const appContext = useContext(AppContext);
+  const tokensInfo = useRef<TokenInfo[]>([]);
+  const self = useRef<HTMLDivElement>(null);
+  const appContext = useContext(AppContext);
 
-    const updateTokens = function(new_text: string) {
-      const [new_tokens, changes] = process_tokens(new_text, tokensInfo.current);
-      tokensInfo.current = new_tokens;
-      setTokenRange(new_tokens.length-1);
-      console.log(new_tokens);
+  const updateTokens = function(new_text: string) {
+    setCurrentInnerText(new_text);
+    const [new_tokens, changes] = process_tokens(new_text, tokensInfo.current);
+    tokensInfo.current = new_tokens;
+    setTokenRange(new_tokens.length-1);
+    console.log(new_tokens);
+  }
+
+  const onKeyDown = function(evt: any) {
+    if(evt.ctrlKey)
+    {
+      if(evt.code === "KeyZ"){
+        evt.preventDefault();
+        appContext?.getCmdHistory().undo_last_command();
+      }
+      if(evt.code === "KeyY"){
+        evt.preventDefault();
+        appContext?.getCmdHistory().redo_last_command();
+      }
     }
+  }
 
-    const onChange = function(evt: any) { updateTokens(evt.target.innerText); }
+  const onChange = function(evt: any) {
+    if(!self || !self.current)
+      return;
 
-    const acceptGrammarSuggestion = function(token: TokenInfo, new_word: string): void {
-      const selection = window.getSelection();
-      if(!self || !self.current || !selection)
-        return;
-      
-      var tk = tokensInfo.current.find(x => x.uuid === token.uuid);
-      if(!tk)
-        return;
-      
-      const select_range = select_token(self.current, tk);
-      selection.removeAllRanges();
-      selection.addRange(select_range);
+    const newText = evt.target.innerText;
+    const writeCmd = new WriteCommand(self.current, currentInnerText, newText, updateTokens);
+    appContext?.getCmdHistory().add_command(writeCmd);
+  }
 
-      if(!selection.focusNode || !selection.focusNode.textContent)
-        return;
-
-      const acceptSuggestionCmd = new AcceptGrammarSuggestionCommand(self.current, selection.focusNode.textContent, token, new_word, updateTokens);
-      appContext?.getCmdHistory().add_command(acceptSuggestionCmd);
-    }
-
-    const test_select = function() {
-      if(self === null || self.current === null)
-        return;
-
-      const token = tokensInfo.current[tokenIndex];
-      const select_range = select_token(self.current, token);
-      setTokenRect(select_range.getBoundingClientRect());
-      console.log(select_range.getBoundingClientRect());
-    }
+  const acceptGrammarSuggestion = function(token: TokenInfo, new_word: string): void {
+    const selection = window.getSelection();
+    if(!self || !self.current || !selection)
+      return;
     
-    const onSliderChange = function(new_val: any) {
-      setTokenIndex(new_val.target.value);
-    }
-
-    let grammar = <></>
-    if(tokenRect && self && self.current) {
-      grammar = 
-      <SuggestGrammarHighlight 
-        rect={tokenRect} 
-        word_suggestion='lorem ipsummmmmasdasdasdsam'
-        word_token={tokensInfo.current[tokenIndex]}
-        accept_grammar_suggestion={acceptGrammarSuggestion}
-        cancel_suggest_grammar={() => setTokenRect(undefined)}/>
-    }
+    var tk = tokensInfo.current.find(x => x.uuid === token.uuid);
+    if(!tk)
+      return;
     
-    return (
-      <TextContainer>
-        <span>
-          <button onClick={() => appContext?.getCmdHistory().undo_last_command()}>Desfazer</button>
-          <button onClick={() => appContext?.getCmdHistory().redo_last_command()}>Refazer</button>
-        </span>
-        {grammar}
-        <TextInput 
-          ref={self} 
-          contentEditable 
-          suppressContentEditableWarning 
-          spellCheck="false"
-          onInput={onChange}>
-        </TextInput>
-        <span>
-          <input type="range" min={0} max={tokenRange} onChange={onSliderChange}></input>
-          <button onClick={test_select}> Test Select </button>
-        </span>
-      </TextContainer>
-    );
+    const select_range = select_token(self.current, tk);
+    selection.removeAllRanges();
+    selection.addRange(select_range);
+
+    if(!selection.focusNode || !selection.focusNode.textContent)
+      return;
+
+    const acceptSuggestionCmd = new AcceptGrammarSuggestionCommand(self.current, selection.focusNode.textContent, token, new_word, updateTokens);
+    appContext?.getCmdHistory().add_command(acceptSuggestionCmd);
+  }
+
+  const test_select = function() {
+    if(self === null || self.current === null)
+      return;
+
+    const token = tokensInfo.current[tokenIndex];
+    const select_range = select_token(self.current, token);
+    setTokenRect(select_range.getBoundingClientRect());
+    console.log(select_range.getBoundingClientRect());
+  }
+  
+  const onSliderChange = function(new_val: any) {
+    setTokenIndex(new_val.target.value);
+  }
+
+  let grammar = <></>
+  if(tokenRect && self && self.current) {
+    grammar = 
+    <SuggestGrammarHighlight 
+      rect={tokenRect} 
+      word_suggestion='lorem ipsummmmmasdasdasdsam'
+      word_token={tokensInfo.current[tokenIndex]}
+      accept_grammar_suggestion={acceptGrammarSuggestion}
+      cancel_suggest_grammar={() => setTokenRect(undefined)}/>
+  }
+  
+  return (
+    <TextContainer>
+      <span>
+        <button onClick={() => appContext?.getCmdHistory().undo_last_command()}>Desfazer</button>
+        <button onClick={() => appContext?.getCmdHistory().redo_last_command()}>Refazer</button>
+      </span>
+      {grammar}
+      <TextInput 
+        ref={self} 
+        contentEditable 
+        suppressContentEditableWarning 
+        spellCheck="false"
+        onInput={onChange}
+        onKeyDown={onKeyDown}>
+      </TextInput>
+      <span>
+        <input type="range" min={0} max={tokenRange} onChange={onSliderChange}></input>
+        <button onClick={test_select}> Test Select </button>
+      </span>
+    </TextContainer>
+  );
 }
 
 export default TextEditor;
