8d2b9b8c9e9f6e81ef0c43af0b7ea1599c61b538
IvanNSBS <insbs@cin.ufpe.br>
2023-07-20 16:14:04 -0300

    Preparing interval for spell checking

--- Entrega 2/front_end_client/src/Views/DocumentEditor/TextEditorTimers.tsx
+++ Entrega 2/front_end_client/src/Views/DocumentEditor/TextEditorTimers.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, FC, useEffect } from "react";
+import { ReactElement, FC, useEffect, useState } from "react";
 import { DocumentController } from "../../Controllers/DocumentController";
 
 type TextEditorTimerProps = {
@@ -10,7 +10,10 @@ type TextEditorTimerProps = {
 export const TextEditorTimers: FC<TextEditorTimerProps> = (props: TextEditorTimerProps) => {
     const controller = props.controller;
     let interval: NodeJS.Timer;
+    let grammarInterval: NodeJS.Timer;
     
+    const [currentContent, setCurrentContent] = useState<string>("");
+
     useEffect(() => {
         interval = setInterval(() => {
             if(!props.documentId) {
@@ -26,9 +29,22 @@ export const TextEditorTimers: FC<TextEditorTimerProps> = (props: TextEditorTime
             const docContent = props.docDiv.current.innerText;
             controller.saveDocument(props.documentId, docContent);
             console.log("Saving document...\n" + docContent);
-        }, 5000);
+        }, 3000);
 
         return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        if(props.docDiv && props.docDiv.current) {
+            setCurrentContent(props.docDiv.current.innerText);
+            console.log("setting current content! " + props.docDiv.current.innerText);
+        }
+
+        grammarInterval = setInterval(() => {
+            console.log("Grammar interval....")
+        }, 8000)
+
+        return () => clearInterval(grammarInterval);
     }, []);
 
     return null;
